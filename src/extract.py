96cacedccfc9fbbd5e59a1d225d46701802051e6
purvac <purvachoudhary@gmail.com>
2024-12-05 17:18:12 -0600

    extractor pulls post_id, submission_title, body, date | update gitignore

--- src/extract.py
+++ src/extract.py
@@ -1,36 +1,30 @@
 import praw
 from datetime import datetime
-#get client_id, client_secret, user_agent, username and password from praw.ini file
-#More information on configuring praw.ini files given here - https://praw.readthedocs.io/en/stable/getting_started/configuration/prawini.html
-reddit = praw.Reddit("DEFAULT") 
+import csv
 
-# if reddit credentials are put in, below statement will be false since your reddit instance is not read only
-print(reddit.read_only)
+SUBMISSION_PULL_LIMIT = 10
 
+# Configure PRAW with credentials from praw.ini file
+reddit = praw.Reddit(config_file='praw.ini')
 
+# Subreddit name
 subreddit_name = "dataengineering"
 subreddit = reddit.subreddit(subreddit_name)
 
-n_of_posts = 0
-try: 
-    new_posts = subreddit.new(limit=10)
-    for post_id in new_posts: 
-        #n_of_posts = n_of_posts + 1
-        print(f"Post ID: {post_id}")
-        submission = reddit.submission(id=post_id)
-        print(f"# of comments: {submission.num_comments}")
-        
-        # Expand all comments and remove 'MoreComments' objects
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list(): 
-            print(f"Comment ID: {comment.id} Comment time: {datetime.utcfromtimestamp(comment.created_utc)}")
-        print("\n")
-        
-        #print(f"Title: {submission.title} | Score:{submission.score} | Created At: {datetime.utcfromtimestamp(submission.created_utc)} | Number of comments: {submission.num_comments} | Text: {submission.selftext}")
-        # the most activity would be measured for new items which receive most number of comments
-        # can we find the time a comment was posted to a post? 
-        #That way we can find the time when most comments are posted
-        #print(f"Post ID: {post_id} Created At: {datetime.utcfromtimestamp(submission.created_utc)} Number of comments: {submission.num_comments}")
-except Exception as e: 
+try:
+    new_posts = subreddit.new(limit=SUBMISSION_PULL_LIMIT)
+    with open('reddit_posts.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Post_ID', 'Title', 'Body', 'Title'])
+        for post_id in new_posts:
+            print(f"Post ID: {post_id}")
+            submission = reddit.submission(id=post_id)
+            print(f"Title: {submission.title}")
+            print(f"Selftext: {submission.selftext}")
+            print(f"Time: {datetime.utcfromtimestamp(submission.created_utc)}")
+            writer.writerow([post_id, submission.title, submission.selftext, datetime.utcfromtimestamp(submission.created_utc)])
+
+except praw.exceptions.RedditAPIException as e:
+    print(f"API Error: {e}")
+except Exception as e:
     print(f"An error occurred: {e}")
-    print("Total number of posts: " + n_of_posts)
