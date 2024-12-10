a5ee278f4c554b4e81a88b2219b15716ebbbad56
purvac <purvachoudhary@gmail.com>
2024-12-10 00:51:49 -0600

    airflow on docker image running - debug find python file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM apache/airflow:2.10.3
 ADD requirements.txt .
 RUN pip install -r requirements.txt
+RUN pip install redis
--- airflow/dags/reddit_etl_dag.py
+++ airflow/dags/reddit_etl_dag.py
@@ -29,20 +29,42 @@ dag = DAG(
     max_active_runs=1,
     tags=["RedditETL"],
 )
-
+"""
 t1 = BashOperator(
-    task_id="checkpoint_task_execution",
+    task_id="checkpoint_1_task_execution",
     bash_command="echo 'First print task completed!'",
     dag=dag,
 )
 
+t2 = BashOperator(
+    task_id="checkpoint_2_task_execution",
+    bash_command="echo 'First print task completed!'",
+    dag=dag,
+)
+
+t2.set_upstream(t1)
+
+"""
+list_files_in_dir = BashOperator(
+    task_id="list_files", 
+    bash_command=f"ls",
+    dag=dag,
+    )
 
 run_script_task = BashOperator(
     task_id="run_extract_py_script", 
     bash_command=f"python /opt/airflow/src/extract_reddit_etl.py",
     dag=dag,
     )
+#ERROR: [2024-12-10T06:20:43.854+0000] {subprocess.py:106} INFO - python: can't open file '/opt/***/src/extract_reddit_etl.py': [Errno 2] No such file or directory
 
-run_script_task.set_upstream(t1)
+run_script_task.set_upstream(list_files_in_dir)
 
+"""
+Commands to run dag: 
+1. docker compose up airflow-init
+2. docker compose up --build
+3. docker ps -> to check if containers are up
+4. docker compose down --volumes --rmi all -> to stop and close all containers
 
+"""
